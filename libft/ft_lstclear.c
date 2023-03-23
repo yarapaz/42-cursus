eeaf95c9d3302e142f249e0b1f3efb546f7d0f79
Yara Paz Gordillo <yapaz-go@c2r10s5.42madrid.com>
2023-03-23 17:57:12 +0100

    clean

--- libft/ft_lstclear.c
+++ libft/ft_lstclear.c
@@ -6,7 +6,7 @@
 /*   By: yapaz-go <yapaz-go@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/22 19:51:42 by yapaz-go          #+#    #+#             */
-/*   Updated: 2023/03/23 16:13:35 by yapaz-go         ###   ########.fr       */
+/*   Updated: 2023/03/23 17:25:09 by yapaz-go         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ void	ft_lstclear(t_list **lst, void (*del)(void *))
 	t_list	*aux;
 	t_list	*aux2;
 
-	if (lst[0] == NULL)
+	if (lst == NULL)
 		return ;
 	else
 	{
--- libft/ft_lstmap.c
+++ libft/ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: yapaz-go <yapaz-go@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/23 16:18:48 by yapaz-go          #+#    #+#             */
-/*   Updated: 2023/03/23 16:48:51 by yapaz-go         ###   ########.fr       */
+/*   Updated: 2023/03/23 17:51:29 by yapaz-go         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 	if (lst == NULL)
 		return (NULL);
 	final_list = ft_lstnew(f(lst->content));
-	if (!final_list)
+	if (final_list == NULL)
 		ft_lstdelone(final_list, del);
 	final_list->next = ft_lstmap(lst->next, f, del);
 	return (final_list);
