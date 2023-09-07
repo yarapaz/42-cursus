9128c4e7a66b8ff9b2cf98d46c99dd2339358933
Yara Paz Gordillo <yapaz-go@c1r9s5.42madrid.com>
2023-09-07 20:35:55 +0200

    final printf development

--- printf/main.c
+++ printf/main.c
@@ -1,27 +1,29 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yapaz-go <yapaz-go@student.42madrid.com>   +#+  +:+       +#+        */
+/*   By: yapaz-go <yapaz-go@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/16 19:49:10 by yapaz-go          #+#    #+#             */
-/*   Updated: 2023/08/09 20:11:10 by yapaz-go         ###   ########.fr       */
+/*   Updated: 2023/09/07 19:25:46 by yapaz-go         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "ft_printf.h"
 #include <stdio.h>
 
 int main(void)
 {
-    int    original;
-    int    copia;
+	int		original;
+    int		feature;
+	// char 	*puntero = "hola";
 
-    original = printf("original %d", 34);
-    copia = ft_printf("copia %d", 34);
+    original = printf(" sdfdfgasfsdf %c%%  sdfasdfasdfasdf %x %d" , 'A', 23423, 7);
+    feature = ft_printf(" sdfdfgasfsdf %c%%  sdfasdfasdfasdf %x %d" , 'A', 23423, 7);
 
-    printf("caracteres original %d", original);
-    printf("caracteres copia %d", copia);
-    
+	printf(" 1) %d ", original);
+	printf(" 2) %d ", feature);
+
     return (0);    
 }
